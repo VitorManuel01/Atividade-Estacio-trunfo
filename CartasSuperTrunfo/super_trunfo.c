3b87c211324f4994476d699f489e3e3175048d18
Vitor Manuel <chorimano.vitor@hotmail>
2025-03-03 14:33:39 -0300

    modified:   CartasSuperTrunfo/output/super_trunfo.exe 	modified:   CartasSuperTrunfo/super_trunfo.c

--- CartasSuperTrunfo/super_trunfo.c
+++ CartasSuperTrunfo/super_trunfo.c
@@ -9,18 +9,20 @@ typedef struct
 
     char codigoCarta[5];
     char nomeCidade[50];
-    int populacao;
+    long int populacao;
     float area;
     float pib;
     int nPontosTuristicos;
     float densidade;
     float pib_percapita;
 
+    float superPoder;
+
 } CartaCidade;
 
 void mostrarMenu()
 {
-    printf("Digite 1 para cadastrar uma carta\nDigite 2 para mostrar cartas cadastradas\nDigite 3 para sair\n\n\nDigitar: ");
+    printf("Digite 1 para cadastrar uma carta\nDigite 2 para mostrar cartas \nDigite 3 para sair\n\n\nDigitar: ");
 }
 
 void gerarCarta(CartaCidade *cartacidade)
@@ -53,14 +55,14 @@ void gerarCarta(CartaCidade *cartacidade)
     system("cls");
 
     printf("Digite a população: ");
-    scanf("%d", &cartacidade->populacao);
+    scanf("%ld", &cartacidade->populacao);
     system("cls");
 
     printf("Digite a área: ");
     scanf("%f", &cartacidade->area);
     system("cls");
 
-    printf("Digite o PIB: ");
+    printf("Digite o PIB em milhões: ");
     scanf("%f", &cartacidade->pib);
     system("cls");
 
@@ -79,11 +81,21 @@ float calcularDensidade(CartaCidade *cartacidade)
         return 0;
     }
 
-    return cartacidade->populacao / cartacidade->area;
+    cartacidade->densidade = cartacidade->populacao / cartacidade->area;
+
+    return cartacidade->densidade;
 }
 
 float calcularPIBPercapita(CartaCidade *cartacidade){
-    return (cartacidade->pib * 1000) / cartacidade->populacao;
+
+    cartacidade->pib_percapita = (cartacidade->pib * 1000) / cartacidade->populacao;
+    return cartacidade->pib_percapita;
+}
+
+float calcularSuperPoder(CartaCidade *cartacidade){
+
+    cartacidade->superPoder =cartacidade->populacao + cartacidade->area + cartacidade->pib + cartacidade->nPontosTuristicos + cartacidade->densidade + cartacidade->pib_percapita;
+    return cartacidade->superPoder;
 }
 
 void mostrarCartas(CartaCidade *cartacidade, int qtdCartas)
@@ -91,26 +103,92 @@ void mostrarCartas(CartaCidade *cartacidade, int qtdCartas)
 
     for (int i = 0; i < qtdCartas; i++)
     {
-        printf("\nCarta Encontrada\n\n");
+        printf("\nCarta: %s\n\n", cartacidade[i].codigoCarta);
 
         printf("Estado: %c\n", cartacidade[i].estado);
 
-        printf("Código: %s\n", cartacidade[i].codigoCarta);
-
         printf("Cidade: %s\n", cartacidade[i].nomeCidade);
 
-        printf("População: %d\n", cartacidade[i].populacao);
+        printf("População: %ld\n", cartacidade[i].populacao);
 
         printf("Área: %.2f km²\n", cartacidade[i].area);
 
         printf("PIB: R$%.2f\n", cartacidade[i].pib);
 
         printf("Pontos Turisticos: %d\n", cartacidade[i].nPontosTuristicos);
+        
+        printf("Densidade Populacional: %.2f hab/km²\n", calcularDensidade(&cartacidade[i]));
 
-        printf("Densidade Populacional: %.2f hab/km²\n", calcularDensidade(cartacidade));
+        printf("PIB per Capita: R$%.2f\n", calcularPIBPercapita(&cartacidade[i]));
 
-        printf("PIB per Capita: R$%.2f\n\n\n", calcularPIBPercapita(cartacidade));
+        printf("Super Poder: %.2f\n\n\n", calcularSuperPoder(&cartacidade[i]));
     }
+
+}
+
+void compararCartas(CartaCidade *cartacidade, int qtdCartas) {
+    if (qtdCartas <= 0) {
+        printf("Nenhuma carta para comparar.\n");
+        return;
+    }
+
+    printf("Comparação de Cartas");
+
+    int maiorIndicePopulacao = 0;
+    for (int i = 1; i < qtdCartas; i++) {
+        if (cartacidade[i].populacao > cartacidade[maiorIndicePopulacao].populacao) {
+            maiorIndicePopulacao = i;
+        }
+    }
+    printf("População: A Carta %d venceu\n", maiorIndicePopulacao + 1);
+
+    int maiorIndiceArea = 0;
+    for (int i = 1; i < qtdCartas; i++) {
+        if (cartacidade[i].area > cartacidade[maiorIndiceArea].area) {
+            maiorIndiceArea = i;
+        }
+    }
+    printf("Área: A Carta %d venceu\n", maiorIndiceArea + 1);
+
+    int maiorIndicePIB = 0;
+    for (int i = 1; i < qtdCartas; i++) {
+        if (cartacidade[i].pib > cartacidade[maiorIndicePIB].pib) {
+            maiorIndicePIB = i;
+        }
+    }
+    printf("PIB: A Carta %d venceu\n", maiorIndicePIB + 1);
+
+    int maiorIndicePontosTuristicos = 0;
+    for (int i = 1; i < qtdCartas; i++) {
+        if (cartacidade[i].nPontosTuristicos > cartacidade[maiorIndicePontosTuristicos].nPontosTuristicos) {
+            maiorIndicePontosTuristicos = i;
+        }
+    }
+    printf("Pontos Turísticos: A Carta %d venceu\n", maiorIndicePontosTuristicos + 1);
+
+    int maiorIndiceDensidade = 0;
+    for (int i = 1; i < qtdCartas; i++) {
+        if (cartacidade[i].densidade > cartacidade[maiorIndiceDensidade].densidade) {
+            maiorIndiceDensidade = i;
+        }
+    }
+    printf("Densidade Populacional: A Carta %d venceu\n", maiorIndiceDensidade + 1);
+
+    int maiorIndicePibPerCapita = 0;
+    for (int i = 1; i < qtdCartas; i++) {
+        if (cartacidade[i].pib_percapita > cartacidade[maiorIndicePibPerCapita].pib_percapita) {
+            maiorIndicePibPerCapita = i;
+        }
+    }
+    printf("PIB per Capita: A Carta %d venceu\n", maiorIndicePibPerCapita + 1);
+
+    int maiorIndiceSuperPoder = 0;
+    for (int i = 1; i < qtdCartas; i++) {
+        if (cartacidade[i].superPoder > cartacidade[maiorIndiceSuperPoder].superPoder) {
+            maiorIndiceSuperPoder = i;
+        }
+    }
+    printf("Super Poder: A Carta %d venceu\n\n\n", maiorIndiceSuperPoder + 1);
 }
 
 int main()
@@ -137,7 +215,7 @@ int main()
         switch (opcao)
         {
         case 1:
-            if (qtdcartas < 5)
+            if (qtdcartas < 3 )
             { // Evita ultrapassar o tamanho do array
                 system("cls");
                 gerarCarta(&cartacidade[qtdcartas]);
@@ -153,7 +231,9 @@ int main()
             system("cls");
             if (qtdcartas > 0)
             {
+                system("cls");
                 mostrarCartas(cartacidade, qtdcartas);
+                compararCartas(cartacidade,qtdcartas);
             }
             else
             {
